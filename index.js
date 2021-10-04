b956ea4fac02b80a921ea656abafa540885ae6a5
Leonard Box <lfbox7@yahoo.com>
2021-10-03 22:51:41 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 8080
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 8080
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/user"));
+const dbo = require("./db/conn");
+app.listen(port, () => {
+    dbo.connectToServer(function (err) {
+        if (err) 
+            console.error(err);
+    });
+    console.log(`Example app listening on port ${port}!`)
+});
